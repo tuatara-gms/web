395660f367835fb5f64fc225e7e95a4657b2898b
Amr <muhammad.amro.moussa@gmail.com>
2020-08-13 17:38:29 +0200

    Prim Docs

---
+++ pages/about/index.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <div class = "about-background" />
+    <div class = "coc-background-bg coc-about-holder">
+      <coc-nav :classes = "{ nav: 'coc-background-bg z-depth-0 coc-border-bottom-1 coc-border-0 coc-border-border coc-full-width' }">
+        <nuxt-link
+          slot = "title"
+          to = "/">
+          <img 
+            src="/snaps/tuatara.png" 
+            class = "tuatara-about-nav-logo col">
+          <div 
+            class = "coc-primary-text col coc-text-bold coc-margin-top-5px" 
+            style="font-family: jura">TUATARA</div>
+        </nuxt-link>
+      </coc-nav>
+      <div class="container">
+        <div class="row">
+          <p class = "coc-text-md-2">
+            <b class = "coc-text-subtitle">What Is Tuatara?</b><br>
+            <b class = "coc-primary-text">Tuatara</b> is a <b>Garage Managment System (GMS)</b> designed to organize the working flow of your garage.
+            We Start from your early operation processes, followed by repairs documentation, reaching Fainancial Status and Analytics.<br>
+            The system is divided into 5 main sections
+            <ol class = "coc-margin-left-2rem">
+              <li><b>Stock Managment</b></li>
+              <li><b>Job Managment</b></li>
+              <li><b>Users Management</b></li>
+              <li><b>Organization Management</b></li>
+              <li><b>Reports and Analytics</b></li>
+            </ol>
+            Each section of them is completes the others, so the final pattern of the system will be the completest GMS ever.<br>
+            <b class = "coc-text-subtitle">How it works</b><br>
+            Every section in Tuatara is designed to be independent, but also completes the others.<br>
+            <b>Organization Managment Section</b><br>
+            <span class="coc-margin-left-2rem">
+              Declares all priamary date your organization may need
+              <ul class = "coc-margin-left-4rem">
+                <li>Organization Data Entiry</li>
+                <li>VAT Declaration</li>
+                <li>Promotions Declaration</li>
+                <li>Smart Card Generation</li>
+                <li>Branches Management</li>
+              </ul>
+            </span>
+            <b>Jobs Section</b><br>
+            <span class="coc-margin-left-2rem">
+              covers the journey of car maintainance using the following sub-modules.
+              <ul class = "coc-margin-left-4rem">
+                <li>Job Registeration</li>
+                <li>Requirements Declaration</li>
+                <li>Operations Record</li>
+                <li>Spare Parts Replacements</li>
+                <li>Labors Registeration</li>
+                <li>Job Status Alteration
+                  <ol class = "coc-margin-left-2rem coc-text-body">
+                    <li>Running</li>
+                    <li>Postponed</li>
+                    <li>Finished</li>
+                  </ol>
+                </li>
+                <li>VAT Applyment</li>
+                <li>Promotions Applyment</li>
+                <li>Job Card Printing</li>
+                <li>Job Qoutation Printing</li>
+                <li>Job Invoice Printing</li>
+                <li>Client History Inquery</li>
+                <li>Car History Inquery</li>
+                <li>Diagnose assistance based on Car History</li>
+              </ul>
+            </span>
+            <b>Stocks Section</b><br>
+            <span class="coc-margin-left-2rem">
+              covers all the imports/exports data flow
+              <ul class = "coc-margin-left-4rem">
+                <li>Parts and Assets Registeration</li>
+                <li>Imports/Exports Moves Execution</li>
+                <li>Providers Support</li>
+                <li>Automobiles Compitability Support</li>
+                <li>External Resources Registeration (Taxes, Salaries, ..etc)</li>
+                <li>Moves listing and deep inqueries</li>
+                <li>Stock listing and deep inqueries</li>
+              </ul>
+            </span>
+            <b class = "coc-text-subtitle">Do i need to do all the work?</b><br>
+            Absloutly not, Tuatara is designed to work with diffirent users and different roles, which makes your data flow smoother and more accurate.<br>
+            Tuatara includes 4 different user roles
+            <ol class = "coc-margin-left-2rem">
+              <li><b>Stocks Supervision</b></li>
+              <li><b>Jobs Supervision</b></li>
+              <li><b>Admin</b></li>
+              <li><b>Owner</b></li>
+            </ol>
+            Every user in your organization can get one or many roles, which makes it flixible for you to manage your staff, but still keep the limits for each one.<br>
+          </p>
+          <div class="col s12 coc-house-keeper">
+            <div class="col l6 s12 coc-house-keeper">
+              <p>
+                <b class = "coc-text-subtitle">What should i expect?</b><br>
+                Tuatara's target is to make the operations easy so you can focus on what's next; That's why we provide you with a super analytics module that will guide you to detect which are your wining cards and which are not.<br>
+                You will have over 20 types of reports with a very deep filtering tools.
+              </p>
+            </div>
+            <div class="col l6 s12">
+              <img
+                src = "/snaps/chartgif.gif"
+                class = "coc-full-width"
+                style = "filter: brightness(1.2) opacity(0.5);">
+            </div>
+          </div>
+          <div class="col s12 coc-house-keeper">
+            <div class="col s12 coc-house-keeper">
+              <img
+                src = "/snaps/COC-trans.png"
+                class = "coc-full-width"
+                style = "height: 100px; width: 100px;" >
+            </div>
+            <div class = "col s12 coc-text-bold pink-text">
+              COC, inc Product
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="css" scoped>
+@font-face {
+  font-family: 'jura';
+  src: url('/fonts/Jura/Jura-Regular.ttf');
+  font-weight: 250;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'jura';
+  src: url('/fonts/Jura/Jura-Bold.ttf');
+  font-weight: bold;
+  font-style: normal;
+}
+.tuatara-about-nav-logo {
+	height: 50px;
+	width: 50px;
+	padding: 0 !important;
+	margin-left: 20px;
+}
+.about-background {
+	background: url(/snaps/compass.png);
+	background-repeat:no-repeat;
+	background-size: contain;
+	background-position: right;
+	height: calc(100vh - 70px);
+	width: 100vw;
+    position: fixed;
+    top: 70px;
+    left: 0;
+	filter: opacity(7%);
+	z-index: 0;
+	max-width: 100vw;
+	overflow-x: hidden;
+}
+.coc-about-holder {
+	min-height: 100vh;
+	width: 99vw;
+	max-width: 99vw;
+	overflow-x: hidden;
+	z-index: 1;
+	display: block;
+	padding-bottom: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,327 +1,102 @@
 <template>
-  <master>
-    <div class="row">
-      <Card 
-        v-if = "user"
-        id = "jobs-create">
+  <div>
+    <div class = "tuatara-commer-main" />
+    <coc-nav :classes = "{ nav: 'transparent' }">
+      <div
+        slot = "title"
+        class="col s12"
+        style = "align-items: center; align-content: center; justify-content: center">
+        <div
+          style="font-family: jura; margin-top: 15px"
+          class = " pink-text">COC, inc Product</div>
+      </div>
+      <div
+        slot = "actions"
+        class="col s12 center">
+        <i-button 
+          to = "/dashboard"
+          type = "text" 
+          size = "large"
+          class = "teal-text text-accent-3 coc-text-bold coc-text-md-2">
+          <span style="font-family: jura">Dashboard</span>
+        </i-button>
+        <i-button 
+          to = "/about"
+          type = "text" 
+          size = "large"
+          class = "teal-text text-accent-3 coc-text-bold coc-text-md-2">
+          <span style="font-family: jura">About Us</span>
+        </i-button>
+      </div>
+    </coc-nav>
+    <div class = "container commer-content-holder">
+      <div class="row">
+        <h1 
+          class = "center coc-text-super coc-text-bold teal-text text-accent-3" 
+          style="font-family: jura;">TUATARA</h1>
         <p 
-          slot = "title">
-          <icon type = " tcsc-transportation-icon coc-text-md-2 coc-content-text"/>
-          <span class="coc-content-text">Create Job</span>
-        </p>
-        <div class="row">
-          <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              :ref = "`${_uid}-phone`"
-              v-model = "input.client.phone"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: { active: true } }"
-              :autocomplete-remote = "model => ({ method: 'get', url: '/job', params: { phone: model.val, limit: 5 }})"
-              :autocomplete-map-response = "res => $_.uniqBy(res.jobs, j => j.client.phone).map(o => o.client.phone)"
-              labeled
-              placeholder = "Client Phone"
-              icon = "ios-phone-portrait"
-              size = "large"
-              light-model
-              allow-autocomplete
-              clearable
-              @coc-enter = "handleAutocompleteSelect($event, 'phone')"
-              @coc-select = "handleAutocompleteSelect($event, 'phone')" />
-          </div>
-          <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              :ref = "`${_uid}-name`"
-              v-model = "input.client.name"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: { active: true } }"
-              :autocomplete-remote = "model => ({ method: 'get', url: '/job', params: { name: model.val, limit: 5 }})"
-              :autocomplete-map-response = "res => $_.uniqBy(res.jobs, j => j.client.name).map(o => o.client.name)"
-              labeled
-              placeholder = "Client Name"
-              icon = "ios-person"
-              size = "large"
-              light-model
-              allow-autocomplete
-              clearable
-              @coc-enter = "handleAutocompleteSelect($event, 'name')"
-              @coc-select = "handleAutocompleteSelect($event, 'name')" />
-          </div>
-          <div class="col l4 m6 s12">
-            <coc-input
-              v-if = "brands && brands.length && isMounted"
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.car.brand"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true }"
-              :data = "brands"
-              :icon = "input.car.brand && input.car.brand.length ? null : 'ios-color-filter'"
-              :style = "input.car.brand && input.car.brand.length ? 'width: calc( 100% - 40px )' : 'width: 100%'"
-              labeled
-              placeholder = "Car Brand"
-              size = "large"
-              class = "col"
-              light-model
-              allow-autocomplete
-              clearable />
-            <coc-avatar
-              v-if = "input.car.brand && input.car.brand.length"
-              slot = "suffix"
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              :source = "`/snaps/brands/png/${input.car.brand.split(' ').join('-').toLowerCase()}.png`"
-              scale = "30px"
-              class = "col coc-margin-left-4px coc-margin-top-25px"/>
-          </div>
-          <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.car.model"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true }"
-              :autocomplete-remote = "model => ({ method: 'get', url: '/job', params: { brand: input && input.car && input.car.brand ? input.car.brand : '' ,model: model.val, limit: 5 }})"
-              :autocomplete-map-response = "res => $_.uniqBy(res.jobs, j => j.car.model).map(o => o.car.model)"
-              labeled
-              placeholder = "Car Model"
-              icon = "ios-car"
-              size = "large"
-              light-model
-              allow-autocomplete
-              clearable />
-          </div>
-          <div class="col l4 m6 s12">
-            <coc-select
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.car.release"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true }"
-              :data = "generateYears()"
-              labeled
-              placeholder = "Release Year"
-              icon = "ios-calendar"
-              size = "large"
-              filterable
-              clearable
-              light-model />
-          </div>
-          <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.car.kilometers"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true, IsNumericString: true, NumberGreaterThan: 0 }"
-              labeled
-              placeholder = "Kilometers"
-              icon = "ios-speedometer"
-              size = "large"
-              light-model
-              clearable />
-          </div>
-          <!-- <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.car.chase"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true }"
-              labeled
-              placeholder = "Chase Number"
-              icon = "ios-barcode"
-              size = "large"
-              light-model />
-          </div> -->
-          <div class="col l4 m6 s12">
-            <coc-input
-              :disabled = "!($utils.roles.hasRole('jobs', user))"
-              v-model = "input.reciptionist"
-              :scope = "['create-job']"
-              :rules = "{ HasValue: true }"
-              labeled
-              placeholder = "Reciptionist"
-              icon = "ios-ionitron"
-              size = "large"
-              light-model
-              clearable />
-          </div>
-          <div class="col l4 m6 s12 right">
-            <button-group class = "coc-margin-top-25px right">
-              <Button
-                v-if = "pullable"
-                type = "success"
-                icon = "ios-time"
-                size = "large"
-                @click = "getHistory">Pull History</Button>
-              <coc-button
-                :disabled = "!($utils.roles.hasRole('jobs', user))"
-                v-model = "btnModel"
-                :scope = "['create-job']"
-                :request = "{ xdata: input, url: '/job' }"
-                :resolve-success-message = "res => `Job #${res.job_no} Created Successfully`"
-                :type = "btnModel && btnModel.meta && btnModel.meta.retriever && btnModel.meta.retriever.errors && btnModel.meta.retriever.errors.response.status === 400 && btnModel.meta.retriever.errors.response.data.includes('#') ? 'error': 'primary'"
-                placeholder = "Create"
-                icon = "ios-add-circle right coc-margin-x-5px"
-                size = "large"
-                reset
-                clearable
-                @coc-submit-accepted = "handleResult"/>
-            </button-group>
-          </div>
-          <div 
-            v-if = "btnModel && btnModel.meta && btnModel.meta.retriever && btnModel.meta.retriever.errors && btnModel.meta.retriever.errors.response.status === 400 && btnModel.meta.retriever.errors.response.data.includes('#')"
-            class = "right coc-house-keeper" >
-            <i-button 
-              :to = "`/jobs/${btnModel.meta.retriever.errors.response.data.match(/[1-9]/g).join('')}`"
-              type = "text"
-              class = "coc-error-text  ">
-              {{ btnModel.meta.retriever.errors.response.data }}
-            </i-button>
-          </div>
+          class = "center teal-text text-accent-3 coc-text-lg coc-text-bold" 
+          style="font-family: jura;">Garage Managment System</p>
+        <div class = "col l6 push-l3">
+          <img 
+            v-coc-mouse-over = "'jello'"
+            v-coc-mouse-leave = "'rubberBand'"
+            src="/snaps/tuatara-teal.png" 
+            class = "coc-full-width animated" >
         </div>
-      </Card>
-      <jobs-history />
-      <Card v-if = "!user">
-        <p class = "coc-text-title coc-error-text center">
-          <Icon type = "ios-alert-outline"/>
-          You Are Not Logged In
-        </p>
-        <br>
-        <p class="center">
-          <button-group>
-            <Button
-              type = "default"
-              size = "large"
-              shape = "circle"
-              style = "width: 120px"
-              @click = "askForLogin">
-              Login
-            </Button>
-          </button-group>
-        </p>
-      </Card>
+      </div>
     </div>
-  </master>
+  </div>
 </template>
 
 <script>
-import Master from '~/components/common/master'
-import JobsHistory from '~/components/jobs/history.vue'
-import brands from '~/plugins/brands'
 export default {
-  name: 'HomeIndex',
-  components: {
-    Master,
-    JobsHistory
-  },
-  head() {
-    return {
-      title: this.$store.state.core.app
-        ? `${this.$store.state.core.app.title} | Home`
-        : 'Home'
-    }
-  },
+  name: 'Index',
+
   data() {
-    return {
-      pullable: false,
-      btnModel: null,
-      isMounted: false,
-      brands,
-      input: {
-        reciptionist: '',
-        car: {
-          brand: '',
-          model: '',
-          release: '',
-          chase: '--',
-          kilometers: ''
-        },
-        client: {
-          name: '',
-          phone: ''
-        }
-      }
-    }
-  },
-  computed: {
-    user() {
-      return this.$store.state.core.auth
-    }
-  },
-  beforeDestroy() {
-    this.$root.$off(['createJob', 'LoggedIn'])
-  },
-  mounted() {
-    setTimeout(() => {
-      this.isMounted = true
-    }, 1000)
-    setTimeout(() => {
-      if (this.user) {
-        this.input.reciptionist = this.user.name
-      }
-    }, 500)
-    this.$root.$on('LoggedIn', p => {
-      setTimeout(() => {
-        if (this.user) {
-          this.input.reciptionist = p.name
-        }
-      }, 500)
-    })
-    this.$root.$on('createJob', p => {
-      setTimeout(() => {
-        this.input = this.$_.cloneDeep(p)
-        this.input.reciptionist = this.user.name
-        setTimeout(() => {
-          if (this.btnModel) {
-            this.btnModel.control.click()
-            setTimeout(() => {
-              const eTarget = document.getElementById('jobs-create')
-              if (eTarget) {
-                eTarget.scrollIntoView()
-              }
-            }, 500)
-          }
-        }, 1500)
-      }, 500)
-    })
-  },
-  methods: {
-    askForLogin() {
-      this.$root.$emit('askForLogin')
-    },
-    handleResult(e) {
-      this.$store.dispatch('setRunningJobs', [
-        ...this.$store.state.jobs.running,
-        e.meta.response
-      ])
-      setTimeout(() => {
-        this.input.reciptionist = this.user.name
-      }, 900)
-    },
-    generateYears() {
-      let i
-      let result = []
-      const year = parseInt(this.$moment().format('YYYY'), 10) + 1
-      for (i = 0; i < 100; i += 1) {
-        result.push((year - i).toString())
-      }
-      return result
-    },
-    getHistory() {
-      this.$root.$emit('getHistory', this.input)
-    },
-    handleAutocompleteSelect(e, ref = 'phone') {
-      this.pullable = false
-      if (!e || !e.length) return
-      const response = this.$refs[
-        `${this._uid}-${ref}`
-      ].autocompleteRetriever.response.jobs.filter(c => c.client[ref] === e)
-      if (response.length) {
-        this.input.client = response[response.length - 1].client
-        this.input.car = response[response.length - 1].car
-        this.pullable = true
-      }
-    }
+    return {}
   }
 }
 </script>
 
 <style lang="css" scoped>
+@font-face {
+  font-family: 'jura';
+  src: url('/fonts/Jura/Jura-Regular.ttf');
+  font-weight: 250;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'jura';
+  src: url('/fonts/Jura/Jura-Bold.ttf');
+  font-weight: bold;
+  font-style: normal;
+}
+.tuatara-commer-main {
+	background: url("/snaps/garage.jpg");
+	background-repeat:no-repeat;
+	background-size: cover;
+	height: 100vh;
+	width: 100vw;
+	filter: grayscale(0.5);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
+.commer-content-holder {
+	height: calc(100vh - 70px);
+	width: 100vw;
+	align-content: center;
+	justify-content: center;
+	align-items: center;
+	display: flex;
+}
+.tuatara-commer-logo-coc {
+	height: 40px;
+	width: 40px;
+	padding: 0 !important;
+	margin-left: 20px;
+	margin-top: 5px;
+}
 </style>
